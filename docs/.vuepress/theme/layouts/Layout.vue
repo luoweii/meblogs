8f11b8bf14b396ab787dc46e0cd741e709cf89a2
luoweii <luowei58@qq.com>
2022-02-15 23:37:31 +0800

    update 1

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  methods: {
+    scrollTo(selector) {
+      if (!selector || selector === '#') return
+      const el = document.querySelector(decodeURIComponent(selector))
+      if (el && el.offsetTop) {
+        window.scrollTo(0, el.offsetTop)
+      }
+    }
+  },
+  mounted() {
+    this.scrollTo(location.hash)
+  }
+}
+</script>
